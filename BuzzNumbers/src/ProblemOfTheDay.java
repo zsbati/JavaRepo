8c0b7b9ce876bab472a0748e4a670b2a1ac068d5
zsbati <zbati123@gmail.com>
2021-11-02 22:06:51 +0000

    11/2

---
+++ BuzzNumbers/src/ProblemOfTheDay.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ProblemOfTheDay {
+
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        switch (str) {
+            case "one":
+                System.out.println("1");
+                break;
+            case "two":
+                System.out.println("2");
+                break;
+            case "three":
+                System.out.println("3");
+                break;
+            case "four":
+                System.out.println("4");
+                break;
+            case "five":
+                System.out.println("5");
+                break;
+            case "six":
+                System.out.println("6");
+                break;
+            case "seven":
+                System.out.println("7");
+                break;
+            case "eight":
+                System.out.println("8");
+        }
+    }
+}
