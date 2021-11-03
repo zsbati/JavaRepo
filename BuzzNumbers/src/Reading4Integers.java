2d0b1191921650708d338853086941070bebc459
zsbati <zbati123@gmail.com>
2021-11-03 21:01:15 +0000

    11/3 1st

---
+++ BuzzNumbers/src/Reading4Integers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Reading4Integers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] numbers = sc.nextLine().split("\s+");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
