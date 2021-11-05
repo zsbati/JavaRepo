4431d008abb5b66facab76d3b544942aff89d4e4
zsbati <zbati123@gmail.com>
2021-11-05 21:35:41 +0000

    11/5 2nd

---
+++ BuzzNumbers/src/StringReadIn.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StringReadIn {
+
+    public static String change(String str) {
+        // write your code here
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != 'a') {
+                out.append(str.charAt(i));
+            } else {
+                out.append('b');
+            }
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(change(str));
+    }
+
+}
+
